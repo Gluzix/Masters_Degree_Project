0478005a35f9ff269c1ce9965930c33bb5ce14bc
Kamil <kamil.gluza@gmail.com>
2022-05-07 00:23:11 +0200

    Added new scripts for reading frames from video, and creating landmarks from images

---
+++ CreateLandmarksFromImages/main.py
@@ -0,0 +1,46 @@
+from PyQt6.QtCore import QDir
+import cv2
+import dlib
+import numpy as np
+import os
+
+if __name__ == '__main__':
+    image_count = 0
+    dir = QDir()
+
+    path_images = dir.currentPath() + "/images/"
+    path_train_src = dir.currentPath() + "/src/"
+    path_train_tar = dir.currentPath() + "/tar/"
+
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(
+        'E:/Projekt Magisterski/resources/pre_trained_data/shape_predictor_68_face_landmarks.dat')
+
+    for filename in os.listdir(path_images):
+        is_found = False
+        f = os.path.join(path_images, filename)
+
+        image = cv2.imread(f)
+
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        rects = detector(gray_image, 1)
+        height, width, channel = image.shape
+
+        blank_image = np.zeros((height, width, 3), np.uint8)
+
+        if rects:
+            is_found = True
+
+        for rect in rects:
+            shape = predictor(gray_image, rect)
+            shape_numpy_arr = np.zeros((68, 2), dtype='int')
+            for i in range(0, 68):
+                shape_numpy_arr[i] = (shape.part(i).x, shape.part(i).y)
+
+            for i, (x, y) in enumerate(shape_numpy_arr):
+                cv2.circle(blank_image, (x, y), 2, (255, 255, 255), -1)
+
+        if is_found:
+            cv2.imwrite(f"{path_train_src}image_{image_count}.png", blank_image)
+            cv2.imwrite(f"{path_train_tar}image_{image_count}.png", image)
+            image_count += 1
