0c120f4fef2027e1bb658e45615a6f724fb62438
Kamil <kamil.gluza@gmail.com>
2022-05-31 21:52:17 +0200

    Added new face detection algorithm

---
+++ FaceDetection/main.py
@@ -0,0 +1,28 @@
+import cv2
+
+imagePath = "E:/Projekt Magisterski/Pix2Pix/tar/image_1.png"
+cascPath = "haarcascade_frontalface_default.xml"
+
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags=cv2.CASCADE_SCALE_IMAGE
+)
+
+print("Found {0} faces!".format(len(faces)))
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x-70, y-70), (x+w+70, y+h+70), (0, 255, 0), 2)
+    cropped_image = image[y-70:y+h+70, x-70:x+w+70]
+    cv2.imshow("Faces found", cropped_image)
+    cv2.waitKey(0)
+
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
